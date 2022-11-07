e5e93ee19870ef5560aa1cdc219d6337d4d9102f
hyunjung-choi <hjchoi.dev@gmail.com>
2022-11-07 23:49:41 +0900

    ✨ Add CHAPTER 2. 변수, 상수, 타입

---
+++ BNR_Kotlin/Sandbox/src/main/kotlin/TypeIntro.kt
@@ -0,0 +1,14 @@
+const val MAX_EXPERIENCE: Int = 5000
+
+fun main(args: Array<String>) {
+    val playerName = "에스트라곤"
+    var experiencePoints = 5
+    experiencePoints += 5
+    var hasSteed = false
+    val barName = "Unicorn's Horn"
+    val barkeeperName = "a barkeeper"
+    val playerGold = 50
+    val barMenu = listOf("벌꿀주", "포도주", "라크루아")
+    println(experiencePoints)
+    println(playerName)
+}
