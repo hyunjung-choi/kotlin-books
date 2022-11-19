2b4c7e37cdf4b7a01d7421fedd9d697b7f11f71c
최현정 <hjchoi.dev@gmail.com>
2022-11-19 12:16:12 +0900

    :sparkles: 챌린지: 아우라 관련 코드 개선하기

---
+++ BNR_Kotlin/NyetHack/src/main/kotlin/challenge/chapter03/Game.kt
@@ -0,0 +1,40 @@
+package challenge.chapter03
+
+fun main(args: Array<String>) {
+    val name = "마드리갈"
+    var healthPoints = 89
+    val isBlessed = true
+    val isImmortal = false
+
+    // 아우라
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    val karma = (Math.pow(Math.random(), (100 - healthPoints) / 100.0) * 20).toInt()
+    val auraColor = if (auraVisible) {
+        when (karma) {
+            in 0..5 -> "RED"
+            in 6..10 -> "ORANGE"
+            in 11..15 -> "PURPLE"
+            in 16..20 -> "GREEN"
+            else -> "NONE"
+        }
+    } else {
+        "NONE"
+    }
+
+    val healthStatus = when (healthPoints) {
+        100 -> "최상의 상태임!"
+        in 90..99 -> "약간의 찰과상만 있음."
+        in 75..89 -> if (isBlessed) {
+            "경미한 상처가 있지만 빨리 치유됨!"
+        } else {
+            "경미한 상처만 있음."
+        }
+        in 15..74 -> "많이 다친 것 같음."
+        else -> "최악의 상태임!"
+    }
+
+    // 플레이어의 상태 출력
+    println("(Aura: ${auraColor}) " +
+            "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+    println("${name} ${healthStatus}")
+}
