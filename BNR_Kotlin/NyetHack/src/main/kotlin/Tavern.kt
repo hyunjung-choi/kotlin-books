96baa73dadd42dd32337b45e7ac9115e77d10e43
최현정 <hjchoi.dev@gmail.com>
2023-04-15 12:15:25 +0900

    ✨ Add CHAPTER 7. 문자열

--- BNR_Kotlin/NyetHack/src/main/kotlin/Tavern.kt
+++ BNR_Kotlin/NyetHack/src/main/kotlin/Tavern.kt
@@ -1,11 +1,38 @@
+const val TAVERN_NAME = "Taernyl's Folly"
 fun main(args: Array<String>) {
-    var beverage = readLine()
+    // placeOrder("shandy,Dragon's Breath,5.91")
+    placeOrder("elixir,Shirley's Temple,4.12")
+}
 
-    beverage?.let {
-        beverage = it.capitalize()
-    } ?: println("beverage가 null입니다!")
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiou]")) {
+        when (it.value) {
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "o" -> "0"
+            "u" -> "|_|"
+            else -> it.value
+        }
+    }
 
-    val beverageServed: String = beverage ?: "맥주"
+private fun placeOrder(menuData: String) {
+    val indexOfApostorphe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostorphe)
+    println("마드리갈은 ${tavernMaster}에게 주문한다.")
 
-    println(beverageServed)
+//    val data = menuData.split(",")
+//    val type = data[0]
+//    val name = data[1]
+//    val price = data[2]
+    val (type, name, price) = menuData.split(",") // 해체선언
+    val message = "마드리갈은 금화 $price 로 $name ($type)를 구입한다."
+    println(message)
+
+    val phrase = if (name == "Dragon's Brath") {
+        "마드리갈이 감탄한다: ${toDragonSpeak("와 $name 진짜 좋구나!")}"
+    } else {
+        "마드리갈이 말한다: 감사합니다 $name."
+    }
+    println(phrase)
 }
