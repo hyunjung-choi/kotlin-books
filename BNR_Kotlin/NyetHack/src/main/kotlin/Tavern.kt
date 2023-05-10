43453cfac230e473b18cffed0ce41a20b6156e31
최현정 <hjchoi.dev@gmail.com>
2023-05-10 21:25:44 +0900

    ✨ Add CHAPTER 8. 숫자

--- BNR_Kotlin/NyetHack/src/main/kotlin/Tavern.kt
+++ BNR_Kotlin/NyetHack/src/main/kotlin/Tavern.kt
@@ -1,7 +1,32 @@
+import kotlin.math.roundToInt
+
 const val TAVERN_NAME = "Taernyl's Folly"
+
+var playerGold = 10
+var playerSilver = 10
+
 fun main(args: Array<String>) {
-    // placeOrder("shandy,Dragon's Breath,5.91")
-    placeOrder("elixir,Shirley's Temple,4.12")
+    placeOrder("shandy,Dragon's Breath,5.91")
+}
+
+fun performPurchase(price: Double) {
+    displayBalance()
+    val totalPurse = playerGold + (playerSilver / 100.0)
+    println("지갑 전체 금액: 금화 $totalPurse")
+    println("금화 $price 로 술을 구입함")
+
+    val remainingBalance = totalPurse - price
+    println("남은 잔액: ${"%.2f".format(remainingBalance)}")
+
+    val remainingGold = remainingBalance.toInt()
+    val remainingSilver = (remainingBalance % 1 * 100).roundToInt()
+    playerGold = remainingGold
+    playerSilver = remainingSilver
+    displayBalance()
+}
+
+fun displayBalance() {
+    println("플레이어의 지갑 잔액: 금화: $playerGold 개, 은화: $playerSilver 개")
 }
 
 private fun toDragonSpeak(phrase: String) =
@@ -29,7 +54,9 @@ private fun placeOrder(menuData: String) {
     val message = "마드리갈은 금화 $price 로 $name ($type)를 구입한다."
     println(message)
 
-    val phrase = if (name == "Dragon's Brath") {
+    performPurchase(price.toDouble())
+
+    val phrase = if (name == "Dragon's Breath") {
         "마드리갈이 감탄한다: ${toDragonSpeak("와 $name 진짜 좋구나!")}"
     } else {
         "마드리갈이 말한다: 감사합니다 $name."
